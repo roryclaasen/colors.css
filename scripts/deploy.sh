369b3a13144a532df3700535c5e9364c6ded5172
Rory Claasen <t-roclaa@microsoft.com>
2017-11-30 13:33:30 +0000

    Updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-BRANCH="node.js"
 now=$(date)
-if [ "$TRAVIS_BRANCH" == "$BRANCH" ]; then
+if [ "$TRAVIS_BRANCH" == "$GIT_BRANCH" ]; then
 	set -o errexit
 
-	git config --global user.email "travis@travis-ci.org"
-	git config --global user.name "Travis CI"
+	git config --global user.email "${GIT_EMAIL}"
+	git config --global user.name "${GIT_NAME}"
     git config --global push.default simple
 
 	git add dist
-	git add package-lock.json
     git add readme.md
-	git commit -m "[ci skip] Built colors.css by Travis CI (Build: $TRAVIS_BUILD_NUMBER at $now)"
-	git push --quiet "https://${GH_TOKEN}@github.com/${GH_REPO}.git" HEAD:$BRANCH
+	git commit -m "[ci skip] Built colors.css (Build: $TRAVIS_BUILD_NUMBER at $$(date))"
+	git push --quiet "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git" HEAD:$GIT_BRANCH
 else
-	echo "Branch is not $BRANCH. Skipping deploy!"
+	echo "Branch is not $GIT_BRANCH. Skipping deploy!"
 fi
