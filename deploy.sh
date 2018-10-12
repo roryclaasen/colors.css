752e9fc11bc495c57e51bce57b1c4616f826ad9b
Rory Claasen <t-roclaa@microsoft.com>
2018-10-12 14:52:33 +0100

    Updated remote

--- deploy.sh
+++ deploy.sh
@@ -8,15 +8,22 @@ if [ "$TRAVIS_BRANCH" == "$GIT_BRANCH" ]; then
     git config --global push.default simple
 
 	git remote set-url origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+	git checkout $GIT_BRANCH
 	git fetch --tags
 
 	git add dist
     git add readme.md
 
+	changed=false
+
+	if ! git diff-index --quiet HEAD --; then
+		changed=true
+	fi
+
 	git commit -m "[ci skip] Built colors.css (Build: $TRAVIS_BUILD_NUMBER at $now)"
 	git push --quiet origin $GIT_BRANCH
 
-	if ! git diff-index --quiet HEAD --; then
+	if [ "$changed" = true]; then
 		version=$(npm version patch)
 
 		git push --quiet origin $version
