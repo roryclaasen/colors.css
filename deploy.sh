339ca54ce55845e8645c8ead8fb5d4f1622af39a
Rory Claasen <t-roclaa@microsoft.com>
2018-10-12 14:43:27 +0100

    Updated remote

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ if [ "$TRAVIS_BRANCH" == "$GIT_BRANCH" ]; then
 	git config --global user.name "${GIT_NAME}"
     git config --global push.default simple
 
-	# git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+	git remote set-url origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 	git fetch --tags
 
 	git add dist
