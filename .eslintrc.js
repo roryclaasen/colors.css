e0a13ec51e0851b89164dff7d26f203fe1a13098
Rory Claasen <rory@xbox.com>
2020-08-24 10:20:31 +0100

    Fix prettier on config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,8 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint', 'import', 'prettier'
-    ],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/typescript',
-        'plugin:prettier/recommended',
-        'prettier',
-        'prettier/@typescript-eslint'
-    ],
+    plugins: ['@typescript-eslint', 'import', 'prettier'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:import/typescript', 'plugin:prettier/recommended', 'prettier', 'prettier/@typescript-eslint'],
     env: {
         node: true
     }
