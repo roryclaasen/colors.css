44f687b8558725765c8f1f28d2f1233ad2d5ab4f
Rory Claasen <rory@xbox.com>
2020-08-24 10:09:27 +0100

    Adds prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,15 @@ module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
     plugins: [
-        '@typescript-eslint',
+        '@typescript-eslint', 'import', 'prettier'
     ],
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:import/typescript',
+        'plugin:prettier/recommended',
+        'prettier',
+        'prettier/@typescript-eslint'
     ],
     env: {
         node: true
